51729afcbe3bb8e1c2d4cfb052193a28227a306d
Leonid Grau <leonid.grau@kit.edu>
2021-06-12 20:56:56 +0200

    disabled starter debugging output file

--- ArkhamDraftStarter/src/main/java/Starter.java
+++ ArkhamDraftStarter/src/main/java/Starter.java
@@ -1,15 +1,17 @@
-import java.io.File;
 import java.io.IOException;
 
 public class Starter {
     public static void startArkhamDraft() throws IOException, InterruptedException {
         ProcessBuilder pb = new ProcessBuilder("java", "-jar", "ArkhamDraft.jar");
 
-        File output = new File("output.txt");
+        //For Debugging
+        /*
+        java.io.File output = new java.io.File("output.txt");
         output.delete();
         output.createNewFile();
         pb.redirectOutput(output);
         pb.redirectError(output);
+        */
         Process p = pb.start();
         int status = p.waitFor();
         System.out.println("Exited with status: " + status);
